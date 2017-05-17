2e477f3dd8a8205fa7bff229648335fbef63536d
김우찬 <teakan7179@gmail.com>
2017-05-17 14:14:04 +0900

    modified score function, depth = 2

--- 201701001.cpp
+++ 201701001.cpp
@@ -2,10 +2,11 @@
 
 /*
 ■아래에 팀정보를 기재하세요.
-대학명: 지역선도대학교
-팀  명: 우리팀
-팀  원: 홍길동(컴퓨터공학과 4학년),...
+대학명: 경북대학교
+팀  명: 내일부터패치
+팀  원: 김우찬(컴퓨터학부 3학년)
 제출일: 2017.5.10
+제출코드: 22702680
 
 ■작성 시 주의사항
 - C++기본 문법에 의하여 작성
@@ -41,74 +42,42 @@ extern int B(int x, int y, int def = OUTBD);
 static int MC;
 static int EC;
 
-int getScore(int value, int color)
-{
-	int weight = 1;
-	if (color == EC)
-		weight = -WEIGHT;
-	switch(value) {
-	case 1:
-		return 10 * weight;
-	case 2:
-		return 40 * weight;
-	case 3:
-		return 160 * weight;
-	case 4:
-		return 640 * weight;
-	case 5:
-		return 10000 * weight;
-	default:
-		return value * weight;
-	}
-}
-
-int getBoundedScore(int value, int color)
-{
-	int weight = 1;
-	if (color == EC)
-		weight = -WEIGHT;
-	switch(value) {
-	case 1:
-		return 0 * weight;
-	case 2:
-		return 1 * weight;
-	case 3:
-		return 2 * weight;
-	case 4:
-		return 3 * weight;
-	case 5:
-		return 100000 * weight;
-	default:
-		return value * weight;
-	}
-}
-
 int getBlockScore(int count, int block, int color)
 {
-	int blockWeight = 2 - block; // block은 0~2의 값이고 크면 역가중치
 	int colorWeight = 1; // 색가중치 기본 = 1
 	int returnValue = 0;
 	if(color == EC)
-		colorWeight = -2;
-	
-	if(count == 1)
-		returnValue = 1 * blockWeight * colorWeight;
-	else if(count == 2)
-		returnValue = 2 * blockWeight * colorWeight;
-	else if(count == 3)
-		returnValue = 8 * blockWeight * colorWeight;
-	else if(count == 4)
-		returnValue = 64 * blockWeight * colorWeight;
-	else if(count == 5)
-		returnValue = 1024 * blockWeight * colorWeight;
-	else {
-		printf("제발 오류 뱉어라\n");
-		returnValue = count * blockWeight * colorWeight;
+		colorWeight = -10;
+
+	if(count == 1) {
+		if(block == 0)
+			returnValue = 3 * colorWeight;
+		if(block == 1)
+			returnValue = 1 * colorWeight;
 	}
-	
+	else if(count == 2) {
+		if(block == 0)
+			returnValue = 12 * colorWeight;
+		if(block == 1)
+			returnValue = 4 * colorWeight;
+	}
+	else if(count == 3) {
+		if(block == 0)
+			returnValue = 48 * colorWeight; // 3개에서 1쪽 막힌거와 안 막힌거의 차이는 크다
+		if(block == 1)
+			returnValue = 16 * colorWeight;
+	}
+	else if(count == 4) {
+		if(block == 0)
+			returnValue = 192 * colorWeight; // 사실상 게임 끝
+		if(block == 1)
+			returnValue = 128 * colorWeight;
+	}
+	else { // count == 5
+		returnValue = 1920 * colorWeight; // 그냥 게임 끝
+	}
 	return returnValue;
 }
-
 class Board {
 private:
 	int board[MAXXY][MAXXY];
@@ -130,13 +99,23 @@ public:
 			}
 		}
 	}
-	int boardValue(int x, int y)
+	int boardValue(int x, int y) // 좌표에 있는 값을 반환(BLACK, WHITE, EMPTY)
 	{
-		// 좌표에 있는 값을 반환(BLACK, WHITE, EMPTY)
 		if((0 <= x && x < MAXXY) && (0 <= y && y < MAXXY)) // 0 ~ 15
 			return board[y][x];
 		else
 			return OUTBD;
+	}
+	bool isNear(const int x, const int y) // 해당 위치 주변에 돌이 있는지를 판단
+	{
+		int _y, _x, dist = 2;
+		for(_y = y - dist; _y <= y + dist; _y++) {
+			for(_x = x - dist; _x <= x + dist; _x++) {
+				if(boardValue(_x, _y) == WHITE || boardValue(_x, _y) == BLACK)
+					return true;
+			}
+		}
+		return false;
 	}
 	void setEval(int _eval)
 	{
@@ -146,193 +125,97 @@ public:
 	{
 		return eval;
 	}
-	void addXY(int x, int y, int color)
+	bool addXY(int _x, int _y, int color) // 새 말을 놓고 newX, newY를 업데이트한다, 5개짜리 돌이 나오면 true 리턴
 	{
-		// 새 말을 놓고 newX, newY를 업데이트한다.
-		board[y][x] = color;
-		new_x = x;
-		new_y = y;
+		board[_y][_x] = color;
+		new_x = _x;
+		new_y = _y;
+		// 바둑알을 놓으면서 5개가 되는지 확인
+		int x, y, i;
+		for (y = -1; y <= MAXXY; y++) {
+			for (x = -1; x <= MAXXY; x++) {
+				int current = boardValue(x, y); // 현재 위치의 상태
+				int myCount, enemy;
+				int right = boardValue(x + 1, y);
+				int rightDown = boardValue(x + 1, y + 1);
+				int down = boardValue(x, y + 1);
+				int downLeft = boardValue(x - 1, y + 1);
+
+				if ((right == color) && current != right) { // right가 바둑알이면서 현재 위치의 상태와 다를 때
+					enemy = (right == WHITE) ? BLACK : WHITE;
+					myCount = 1;
+					for(i = 2; i <= 5; i++) {
+						int tempVal = boardValue(x + i, y);
+						if(tempVal == OUTBD || tempVal == enemy) {
+							break;
+						}
+						if(tempVal == EMPTY)
+							break;
+						myCount++;
+					}
+					if(myCount == 5)
+						return true;
+				}
+				if ((rightDown == color) && current != rightDown) { // right-down가 바둑알이면서 현재 위치의 상태와 다를 때
+					enemy = (rightDown == WHITE) ? BLACK : WHITE;
+					myCount = 1;
+					for(i = 2; i <= 5; i++) {
+						int tempVal = boardValue(x + i, y + i);
+						if(tempVal == OUTBD || tempVal == enemy) {
+							break;
+						}
+						if(tempVal == EMPTY)
+							break;
+						myCount++;
+					}
+					if(myCount == 5)
+						return true;
+				}
+				if ((down == color) && current != down) { // down가 바둑알이면서 현재 위치와는 다를 때
+					enemy = (down == WHITE) ? BLACK : WHITE;
+					myCount = 1;
+					for(i = 2; i <= 5; i++) {
+						int tempVal = boardValue(x, y + i);
+						if(tempVal == OUTBD || tempVal == enemy) {
+							break;
+						}
+						if(tempVal == EMPTY)
+							break;
+						myCount++;
+					}
+					if(myCount == 5)
+						return true;
+				}
+				if ((downLeft == color) && current != downLeft) { // down-left가 바둑알이면서 현재 위치와는 다를 때
+					enemy = (downLeft == WHITE) ? BLACK : WHITE;
+					myCount = 1;
+					for(i = 2; i <= 5; i++) {
+						int tempVal = boardValue(x - i, y + i);
+						if(tempVal == OUTBD || tempVal == enemy) {
+							break;
+						}
+						if(tempVal == EMPTY)
+							break;
+						myCount++;
+					}
+					if(myCount == 5)
+						return true;
+				}
+			}
+		}
+		return false;
 	}
 	void setXY(int x, int y)
 	{
 		new_x = x;
 		new_y = y;
 	}
-	void getXY(int &x, int &y)
+	void getXY(int *x, int *y)
 	{
-		x = new_x;
-		y = new_y;
+		*x = new_x;
+		*y = new_y;
 	}
-	void evaluation() // evaluation function
-	{
-		int x, y, i;
-		int tempEval = 0;
-		// 바둑판에 배치된 모든 말에 대해 검사한다.
-		for (y = 0; y < MAXXY; y++) {
-			for (x = 0; x < MAXXY; x++) {
-				int currentColor = board[y][x]; // 현재 위치의 색깔
-				if (currentColor == WHITE && currentColor == BLACK) { // 빈 공간이 아닐 경우
-					int enemyColor = (currentColor == WHITE) ? BLACK : WHITE;
-
-					int tempValue = 1;
-					bool isBlocked = false;
-					for (i = 1; i <= 4; i++) { // up
-						if (y - i < 0) { // 바둑판 바깥으로 넘어가면
-							isBlocked = true;
-							break;
-						}
-						if (board[y - i][x] != currentColor) {
-							isBlocked = true;
-							break;
-						}
-						if (board[y - i][x] == currentColor) // 내 말이면 임시점수 + 1
-							tempValue++;
-					}
-					if (isBlocked)
-						tempEval += getBoundedScore(tempValue, currentColor);
-					else
-						tempEval += getScore(tempValue, currentColor);
-
-
-					tempValue = 1;
-					isBlocked = false;
-					for (i = 1; i <= 4; i++) { // up-right
-						if (y - i < 0 || x + i >= MAXXY) { // 바둑판 바깥으로 넘어가면
-							isBlocked = true;
-							break;
-						}
-						if (board[y - i][x + i] != currentColor) {
-							isBlocked = true;
-							break;
-						}
-						if (board[y - i][x + i] == currentColor) // 내 말이면 임시점수 + 1
-							tempValue++;
-					}
-					if (isBlocked)
-						tempEval += getBoundedScore(tempValue, currentColor);
-					else
-						tempEval += getScore(tempValue, currentColor);
-
-					tempValue = 1;
-					isBlocked = false;
-					for (i = 1; i <= 4; i++) { // right
-						if (x + i >= MAXXY) { // 바둑판 바깥으로 넘어가면
-							isBlocked = true;
-							break;
-						}
-						if (board[y][x + i] != currentColor) {
-							isBlocked = true;
-							break;
-						}
-						if (board[y][x + i] == currentColor) // 내 말이면 임시점수 + 1
-							tempValue++;
-					}
-					if (isBlocked)
-						tempEval += getBoundedScore(tempValue, currentColor);
-					else
-						tempEval += getScore(tempValue, currentColor);
-
-					tempValue = 1;
-					isBlocked = false;
-					for (i = 1; i <= 4; i++) { // right-down
-						if (x + i >= MAXXY || y + i >= MAXXY) { // 바둑판 바깥으로 넘어가면
-							isBlocked = true;
-							break;
-						}
-						if (board[y + i][x + i] != currentColor) {
-							isBlocked = true;
-							break;
-						}
-						if (board[y + i][x + i] == currentColor) // 내 말이면 임시점수 + 1
-							tempValue++;
-					}
-					if (isBlocked)
-						tempEval += getBoundedScore(tempValue, currentColor);
-					else
-						tempEval += getScore(tempValue, currentColor);
-
-					tempValue = 1;
-					isBlocked = false;
-					for (i = 1; i <= 4; i++) { // down
-						if (y + i >= MAXXY) { // 바둑판 바깥으로 넘어가면
-							isBlocked = true;
-							break;
-						}
-						if (board[y + i][x] != currentColor) {
-							isBlocked = true;
-							break;
-						}
-						if (board[y + i][x] == currentColor) // 내 말이면 임시점수 + 1
-							tempValue++;
-					}
-					if (isBlocked)
-						tempEval += getBoundedScore(tempValue, currentColor);
-					else
-						tempEval += getScore(tempValue, currentColor);
-
-					tempValue = 1;
-					isBlocked = false;
-					for (i = 1; i <= 4; i++) { // down-left
-						if (y + i >= MAXXY || x - i < 0) { // 바둑판 바깥으로 넘어가면
-							isBlocked = true;
-							break;
-						}
-						if (board[y + i][x - i] != currentColor) {
-							isBlocked = true;
-							break;
-						}
-						if (board[y + i][x - i] == currentColor) // 내 말이면 임시점수 + 1
-							tempValue++;
-					}
-					if (isBlocked)
-						tempEval += getBoundedScore(tempValue, currentColor);
-					else
-						tempEval += getScore(tempValue, currentColor);
-
-					tempValue = 1;
-					isBlocked = false;
-					for (i = 1; i <= 4; i++) { // left
-						if (x - i < 0) { // 바둑판 바깥으로 넘어가면
-							isBlocked = true;
-							break;
-						}
-						if (board[y][x - i] != currentColor) {
-							isBlocked = true;
-							break;
-						}
-						if (board[y][x - i] == currentColor) // 내 말이면 임시점수 + 1
-							tempValue++;
-					}
-					if (isBlocked)
-						tempEval += getBoundedScore(tempValue, currentColor);
-					else
-						tempEval += getScore(tempValue, currentColor);
-
-					tempValue = 1;
-					isBlocked = false;
-					for (i = 1; i <= 4; i++) { // left - up
-						if (x - i < 0 || y - i < 0) { // 바둑판 바깥으로 넘어가면
-							isBlocked = true;
-							break;
-						}
-						if (board[y - i][x - i] != currentColor) {
-							isBlocked = true;
-							break;
-						}
-						if (board[y - i][x - i] == currentColor) // 내 말이면 임시점수 + 1
-							tempValue++;
-					}
-					if (isBlocked)
-						tempEval += getBoundedScore(tempValue, currentColor);
-					else
-						tempEval += getScore(tempValue, currentColor);
-				}
-			}
-		}
-		eval = tempEval;
-	}
-
-	void evaluation2()
+	void evaluation(int depth)
 	{
 		int x, y, i;
 		int tempEval = 0;
@@ -420,7 +303,7 @@ public:
 				}
 			}
 		}
-		eval = tempEval;
+		eval = tempEval * depth * 2;
 	}
 };
 
@@ -443,8 +326,7 @@ int minNode(int a, int b)
 Board minimax(Board node, int depth, int alpha, int beta, bool maximizingPlayer)
 {
 	if (depth == 0) {
-		node.evaluation2();
-		int temp = node.getEval();
+		node.evaluation(1);
 		return node;
 	}
 
@@ -455,24 +337,29 @@ Board minimax(Board node, int depth, int alpha, int beta, bool maximizingPlayer)
 	// xy값은 반환받은 노드의 것을 사용하면 안 되고 현 함수의 ij를 사용해야함
 
 	if (maximizingPlayer) { // (최대값을 고르는) 내 턴일 경우
-		int bestValue = -99999;
+		int bestValue = -99999999, v;
 		int bestX = 0, bestY = 0;
 		for (i = 0; i < MAXXY; i++) {
 			for (j = 0; j < MAXXY; j++) {
 				// 해당 칸이 빈칸인 경우에만 트리를 생성
-				if (node.boardValue(i, j) == EMPTY) {
+				if (node.boardValue(i, j) == EMPTY && node.isNear(i, j)) {
 					Board nextNode = node; // minmax 함수에 들어감 임시 노드
-					nextNode.addXY(i, j, MC); // xy 경우의 수 대입
 
-					// 반환받은 노드의 eval값만 사용하고 나머지는 버림(어차피 x, y값은 첫 함수의 값을 반환해야하기때문에)
-					int v = minimax(nextNode, depth - 1, alpha, beta, false).getEval();
-
+					if(nextNode.addXY(i, j, MC)) { // xy 경우의 수 대입(5개짜리 돌이 있으면 true)
+						nextNode.evaluation(depth);
+						v = nextNode.getEval();
+						printf("five in a raw\n");
+					}
+					else {
+						// 반환받은 노드의 eval값만 사용하고 나머지는 버림(어차피 x, y값은 첫 함수의 값을 반환해야하기때문에)
+						v = minimax(nextNode, depth - 1, alpha, beta, false).getEval();
+					}
 					bestValue = maxNode(bestValue, v);
 					if (bestValue == v) {
 						bestX = i;
 						bestY = j;
 					}
-					/*
+
 					// alpha-beta pruning
 					alpha = maxNode(alpha, bestValue);
 					if (beta <= alpha) {
@@ -480,7 +367,7 @@ Board minimax(Board node, int depth, int alpha, int beta, bool maximizingPlayer)
 						node.setXY(bestX, bestY);
 						return node;
 					}
-					*/
+
 				}
 			}
 		}
@@ -490,23 +377,29 @@ Board minimax(Board node, int depth, int alpha, int beta, bool maximizingPlayer)
 		return node;
 	}
 	else { // minimizingPlayer
-		int bestValue = 99999;
+		int bestValue = 99999999, v;
 		int bestX = 0, bestY = 0;
 		for (i = 0; i < MAXXY; i++) {
 			for (j = 0; j < MAXXY; j++) {
 				// 해당 칸이 빈칸인 경우에만 트리를 생성
-				if (node.boardValue(i, j) == EMPTY) {
+				if (node.boardValue(i, j) == EMPTY && node.isNear(i, j)) {
 					Board nextNode = node; // minmax 함수에 들어감 임시 노드
-					nextNode.addXY(i, j, EC); // 임시 노드의 xy 좌표에 돌을 놓음
 
-					int v = minimax(nextNode, depth - 1, alpha, beta, true).getEval();
-
+					if(nextNode.addXY(i, j, EC)) { // xy 경우의 수 대입(5개짜리 돌이 있으면 true)
+						nextNode.evaluation(depth);
+						v = nextNode.getEval();
+						printf("five in a raw\n");
+					}
+					else {
+						// 반환받은 노드의 eval값만 사용하고 나머지는 버림(어차피 x, y값은 첫 함수의 값을 반환해야하기때문에)
+						v = minimax(nextNode, depth - 1, alpha, beta, true).getEval();
+					}
 					bestValue = minNode(bestValue, v);
 					if (bestValue == v) {
 						bestX = i;
 						bestY = j;
 					}
-					/*
+
 					// alpha-beta pruning
 					beta = minNode(beta, bestValue);
 					if (beta <= alpha) {
@@ -514,7 +407,6 @@ Board minimax(Board node, int depth, int alpha, int beta, bool maximizingPlayer)
 						node.setXY(bestX, bestY);
 						return node;
 					}
-					*/
 				}
 			}
 		}
@@ -530,13 +422,15 @@ void f201701001(int *NewX, int *NewY, int mc, int CurTurn)
 	MC = mc;
 	EC = ec;
 
+	srand(time(NULL));
+
 	Board current_board;
 	current_board.initBoard(); // 현재 판으로 초기화
 
-	int alpha = -99999, beta = 99999;
-	Board new_board = minimax(current_board, 1, alpha, beta, true);
+	int alpha = -99999999, beta = 99999999;
+	Board new_board = minimax(current_board, 2, alpha, beta, true);
 
-	new_board.getXY(*NewX, *NewY);
+	new_board.getXY(NewX, NewY);
 
 	if (CurTurn == 0) {
 		*NewX = rand() % MAXXY;
